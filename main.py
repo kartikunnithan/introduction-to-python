4499c627f28d24f32f4d7777afa4de820d31f51e
kartikunnithan <kartikunnithan738@gmail.com>
2025-02-15 13:43:04 +0400

    1

---
+++ main.py
@@ -0,0 +1,12 @@
+print("Welcome to Python")
+print("my name is Kartik Unnithan")
+print("i am 12 years old and i go to dukesbrodge secondary school in  Mauritius ")
+#i am printing a brief explanation about me
+print("i love playing football and my idolis Cristiano Ronaldo")
+#ronaldo is the goat!
+#he is the best
+
+print("the sum of 8 and 12 is;",8+12)
+print("when you subtract 23 and 16 you get;",23-16)
+print("When you divide 81 and 22 you get;",81/22)
+print("the product of 46 and 57 is;",46*57)
